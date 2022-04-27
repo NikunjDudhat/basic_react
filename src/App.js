2e131c71c353a76f7d12ca4dff817bdec1af7a15
RADHIKAKACHHADIYA <radhikakachhadiya823@gmail.com>
2022-04-27 11:00:05 +0530

    let and const

--- src/App.js
+++ src/App.js
@@ -2,6 +2,51 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  // let a  = 20;
+  // {
+  //   let a = 10;
+  // }
+  // console.log(a);
+
+
+  // const a = 10;
+  // {
+  //   const a = 20;
+  // }
+  // console.log(a);
+
+
+  // const value compulsory declare
+  // const a;
+  // {
+  //   const a = 20;
+  // }
+  // console.log(a);
+
+  // let a =10;
+  // a=a + 2;
+  // console.log(a);
+
+  // const value can not be re-declared 
+  // const a = 10;    
+  // a = a +10;
+  // console.log(a);
+
+  // Arrow function
+
+  // let arr = [1,2,3,4,5]
+  // arr.map((a,i) => console.log(a, i));
+  // (a,i) ==>  a for value and i for index
+
+
+  // let function_name = () => {
+  //   console.log("Hello");
+  // }
+  // function_name();
+  
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -15,7 +60,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React hello nikunj
+          Learn React
         </a>
       </header>
     </div>
