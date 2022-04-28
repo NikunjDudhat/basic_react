8916e0c6b3742017776ebac2be83b87edb7bb5d7
RADHIKAKACHHADIYA <radhikakachhadiya823@gmail.com>
2022-04-28 10:55:55 +0530

    Array

--- src/App.js
+++ src/App.js
@@ -45,6 +45,45 @@ function App() {
   // }
   // function_name();
   
+  
+  let a = [1,2,3,4,5,6];
+  let b = [1,2,3,4,5,6];
+  let c = [1,2,3,4,5,6];
+  let d = [1,2,3,4,5,6];
+  let e = [1,2,3,4,5,6];
+  let f = [1,2,3,4,5,6];
+  let g = [1,2,3,4,5,6];
+  let h = [1,2,3,4,5,6];
+  let i = [1,2,3,4,5,6];
+  let j = [1,2,3,4,5,6];
+
+
+  a.push(99);
+  b.unshift(99);
+  c.pop();
+  d.shift();
+  e.splice(1,0,7,8);
+  f.splice(1,2,7,8);
+  g.toString(1,2,7,8);
+  let res = h.some((a) => a > 5);
+  let re = i.find((a) => a >1)
+  let result = Array.isArray(j)
+  
+
+  console.log(a, "Add Element at lest");
+  console.log(b, "Add Eleement at first");
+  console.log(c, "Remoce lement at last");
+  console.log(d, "Remoce lement at First");
+  console.log(e, "Add Element at Splice Add");
+  console.log(f, "Add Element at Splice Remove");
+  console.log(g, "Convert Array to string");
+  console.log(res, "Check within array return true/flase");
+  console.log(re, "Check within array return first match Element ");
+  console.log(result, "Check Given Element in Array into String");
+
+
+
+
 
 
   return (
